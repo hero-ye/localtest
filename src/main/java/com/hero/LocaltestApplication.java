7871ec9322d7d039957ad2ef10351c553af055c0
yejinxiong <yejinxiong01@126.com>
2020-11-27 00:15:55 +0800

    不使用springboot测试模块

--- src/main/java/com/hero/LocaltestApplication.java
+++ src/main/java/com/hero/LocaltestApplication.java
@@ -3,9 +3,11 @@ package com.hero;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Log
+@EnableSwagger2
 public class LocaltestApplication {
 
     public static void main(String[] args) {
